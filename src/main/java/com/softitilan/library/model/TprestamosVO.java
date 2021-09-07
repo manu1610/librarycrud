c8dd79c8f01651bafd80be141f29641175238b0b
Emmanuel Hernandez Ramirez <emmanuel.ramirez@gnp.com.mx>
2021-09-07 12:00:46 -0500

    [sxpjb] correciones de identacion y interface

--- src/main/java/com/softitilan/library/model/TprestamosVO.java
+++ src/main/java/com/softitilan/library/model/TprestamosVO.java
@@ -1,4 +1,5 @@
 package com.softitilan.library.model;
+
 import java.io.Serializable;
 import java.util.Date;
 
@@ -7,17 +8,18 @@ import javax.persistence.*;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Entity
 @Table(name = "tprestamos")
-public class TprestamosVO {
-	@Id
+public class TprestamosVO implements Serializable {
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer Id;
     @ManyToOne
-    @JoinColumn(name = "idlector", referencedColumnName = "id")
+    @JoinColumn(name = "id_lector", referencedColumnName = "id")
     private TlectorVO idLector;
     @ManyToOne
-    @JoinColumn(name = "idLibro", referencedColumnName = "id")
+    @JoinColumn(name = "id_libro", referencedColumnName = "id")
     private TlibroVO idLibro;
     @Temporal(TemporalType.DATE)
     @JsonFormat(pattern = "yyyy-MM-dd")
@@ -37,73 +39,96 @@ public class TprestamosVO {
     @JsonFormat(pattern = "yyyy-MM-dd")
     private Date modifiedAt;
     private int modifiedBy;
-	public Integer getId() {
-		return Id;
-	}
-	public void setId(Integer id) {
-		Id = id;
-	}
-	public TlectorVO getIdLector() {
-		return idLector;
-	}
-	public void setIdLector(TlectorVO idLector) {
-		this.idLector = idLector;
-	}
-	public TlibroVO getIdLibro() {
-		return idLibro;
-	}
-	public void setIdLibro(TlibroVO idLibro) {
-		this.idLibro = idLibro;
-	}
-	public Date getFechaPrestamo() {
-		return fechaPrestamo;
-	}
-	public void setFechaPrestamo(Date fechaPrestamo) {
-		this.fechaPrestamo = fechaPrestamo;
-	}
-	public Date getFechaEntregaEsperada() {
-		return fechaEntregaEsperada;
-	}
-	public void setFechaEntregaEsperada(Date fechaEntregaEsperada) {
-		this.fechaEntregaEsperada = fechaEntregaEsperada;
-	}
-	public Date getFechaEntregaRealizada() {
-		return fechaEntregaRealizada;
-	}
-	public void setFechaEntregaRealizada(Date fechaEntregaRealizada) {
-		this.fechaEntregaRealizada = fechaEntregaRealizada;
-	}
-	public int getStatus() {
-		return status;
-	}
-	public void setStatus(int status) {
-		this.status = status;
-	}
-	public Date getCreatedAt() {
-		return createdAt;
-	}
-	public void setCreatedAt(Date createdAt) {
-		this.createdAt = createdAt;
-	}
-	public int getCreatedBy() {
-		return createdBy;
-	}
-	public void setCreatedBy(int createdBy) {
-		this.createdBy = createdBy;
-	}
-	public Date getModifiedAt() {
-		return modifiedAt;
-	}
-	public void setModifiedAt(Date modifiedAt) {
-		this.modifiedAt = modifiedAt;
-	}
-	public int getModifiedBy() {
-		return modifiedBy;
-	}
-	public void setModifiedBy(int modifiedBy) {
-		this.modifiedBy = modifiedBy;
-	}
-	@Override
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public TlectorVO getIdLector() {
+        return idLector;
+    }
+
+    public void setIdLector(TlectorVO idLector) {
+        this.idLector = idLector;
+    }
+
+    public TlibroVO getIdLibro() {
+        return idLibro;
+    }
+
+    public void setIdLibro(TlibroVO idLibro) {
+        this.idLibro = idLibro;
+    }
+
+    public Date getFechaPrestamo() {
+        return fechaPrestamo;
+    }
+
+    public void setFechaPrestamo(Date fechaPrestamo) {
+        this.fechaPrestamo = fechaPrestamo;
+    }
+
+    public Date getFechaEntregaEsperada() {
+        return fechaEntregaEsperada;
+    }
+
+    public void setFechaEntregaEsperada(Date fechaEntregaEsperada) {
+        this.fechaEntregaEsperada = fechaEntregaEsperada;
+    }
+
+    public Date getFechaEntregaRealizada() {
+        return fechaEntregaRealizada;
+    }
+
+    public void setFechaEntregaRealizada(Date fechaEntregaRealizada) {
+        this.fechaEntregaRealizada = fechaEntregaRealizada;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public int getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(int createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getModifiedAt() {
+        return modifiedAt;
+    }
+
+    public void setModifiedAt(Date modifiedAt) {
+        this.modifiedAt = modifiedAt;
+    }
+
+    public int getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(int modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+
+    @Override
     public String toString() {
         ObjectMapper mapper = new ObjectMapper();
         try {
